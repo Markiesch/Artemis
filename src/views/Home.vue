8ad934f6461ad5b3a5b193bc73da7e201be10f4a
Markiesch <marksmeel@gmail.com>
2022-02-21 22:16:38 +0100

    Add homepage template

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>Homepage</h1>
+  <section class="hero">
+    <h1>Geen enkel dier is overbodig</h1>
+    <p>Ieder dier in nood is welkom in onze opvang. Wij vinden dat ieder dier geholpen moet worden, ongeacht waar het dier geboren is, een ziekte of handicap heeft.</p>
+
+    <router-link :to="{ name: 'Home' }">Verander nu een leven</router-link>
+  </section>
 </template>
+
+<style lang="scss">
+.hero {
+  min-height: calc(100vh - var(--nav-height) * 2);
+
+  & > * {
+    max-width: 40rem;
+  }
+
+  a {
+    color: var(--primary-color);
+  }
+}
+</style>
