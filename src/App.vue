61336136265d9994e211f0b271ddcfa2047fc789
Markiesch <marksmeel@gmail.com>
2022-02-21 22:16:20 +0100

    Added scroll variables

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,43 @@
 
 <script setup lang="ts">
 import Navigation from "./components/Navigation.vue";
+
+const root = document.documentElement;
+
+function updateScroll() {
+  const scrollTop = window.scrollY || document.documentElement.scrollTop;
+  root.style.setProperty("--scroll", -scrollTop + "px");
+}
+
+updateScroll();
+window.addEventListener("scroll", updateScroll);
 </script>
 
-<style>
+<style lang="scss">
+:root {
+  --primary-color: #eb7252;
+  --secondary-color: #144739;
+  --text-color: hsla(0, 0%, 0%, 0.7);
+  --nav-height: 5rem;
+  --scroll: 0;
+}
+
 * {
   margin: 0;
   padding: 0;
   box-sizing: border-box;
   font-family: "Poppins", sans-serif;
+}
+
+nav,
+section {
+  max-width: 90rem;
+  margin: 0 auto;
+  padding: 0 1rem;
+}
+
+a {
+  text-decoration: none;
+  color: inherit;
 }
 </style>
