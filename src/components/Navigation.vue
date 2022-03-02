de26f24c06471e24cad61dc50b329d7f6c7c1819
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-02 17:18:31 +0100

    Made logo router-link

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav>
-    <div>Artemis</div>
+    <router-link :to="{ name: 'Home' }" class="logo">Artemis</router-link>
     <div class="link--container">
       <router-link :to="{ name: 'Home' }">Home</router-link>
       <router-link :to="{ name: 'About' }">Overzicht</router-link>
@@ -13,10 +13,20 @@
 
 <style lang="scss">
 nav {
+  position: fixed;
+  z-index: 2;
+  top: 0;
+  left: 0;
+  right: 0;
   display: flex;
   justify-content: space-between;
   align-items: center;
   height: var(--nav-height);
+  font-size: 1.25rem;
+
+  .logo {
+    font-weight: 600;
+  }
 
   .link--container {
     a {
