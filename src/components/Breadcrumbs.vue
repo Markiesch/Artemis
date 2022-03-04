61949772d79f6f6e41c234c81ec1d01f3933a559
Markiesch <marksmeel@gmail.com>
2022-03-04 19:37:55 +0100

    Added breadcrumbs

---
+++ src/components/Breadcrumbs.vue
@@ -0,0 +1,55 @@
+<template>
+  <section>
+    <div class="breadcrumbs">
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <svg viewBox="0 0 192 512">
+        <path d="M0 384.662V127.338c0-17.818 21.543-26.741 34.142-14.142l128.662 128.662c7.81 7.81 7.81 20.474 0 28.284L34.142 398.804C21.543 411.404 0 402.48 0 384.662z"></path>
+      </svg>
+      <span><slot /></span>
+    </div>
+    <h2><slot /></h2>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+@import "../styles/variables.scss";
+
+section {
+  padding: 0.5rem;
+}
+
+.breadcrumbs {
+  display: flex;
+  align-items: center;
+  margin-bottom: 0.5rem;
+
+  a,
+  span {
+    padding: 0.5rem;
+  }
+
+  svg {
+    height: 1rem;
+    transition: transform 300ms cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  }
+
+  .home--link {
+    font-weight: 600;
+
+    &:hover + svg {
+      transform: rotate(180deg);
+    }
+  }
+
+  span,
+  svg {
+    color: $grey-color;
+    fill: currentColor;
+  }
+}
+
+h2 {
+  line-height: 1;
+  padding-left: 0.5rem;
+}
+</style>
