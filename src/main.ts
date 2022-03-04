702dfe4e21e21defbfa35137dd8ddd042673047b
Markiesch <marksmeel@gmail.com>
2022-03-04 19:37:48 +0100

    Added sass import files

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import Router from "./router";
+import "./styles/variables.scss";
+import "./styles/typography.scss";
 
 createApp(App).use(Router).mount("body");
