1fee1a8076319192356104940a7232f556c15d23
Markiesch <marksmeel@gmail.com>
2022-03-05 21:27:17 +0100

    Added article view

---
+++ src/views/Article.vue
@@ -0,0 +1,16 @@
+<template>
+  <section v-if="article">
+    <h2>{{ article.title }}</h2>
+  </section>
+</template>
+
+<script setup lang="ts">
+import { useRoute, useRouter } from "vue-router";
+import { articles } from "../data/articles";
+
+const route = useRoute();
+const router = useRouter();
+
+const article = articles.find((item) => item.id === +route.params.id);
+if (!article) router.push({ name: "404" });
+</script>
