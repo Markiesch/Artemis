3a9f774709a1a3a60d0b09d2df2e5adecabf2b5d
Markiesch <marksmeel@gmail.com>
2022-03-05 21:26:55 +0100

    Added newscard component

---
+++ src/components/NewsCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <article v-if="article">
+    <h2>{{ article.title }}</h2>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { articles } from "../data/articles";
+
+const { id } = defineProps<{ id: number }>();
+
+const article = articles.find((item) => item.id === id);
+</script>
