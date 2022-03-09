5da92ac9d90993ca8cf6c5fa69d9391538de43a1
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-09 18:30:27 +0100

    Added newscard component

--- src/components/NewsCard.vue
+++ src/components/NewsCard.vue
@@ -1,6 +1,11 @@
 <template>
-  <article v-if="article">
-    <h2>{{ article.title }}</h2>
+  <article class="news--card" v-if="article">
+    <img :src="`/assets/${article.image}`" :alt="article.title" />
+    <div class="details">
+      <h2>{{ article.title }}</h2>
+      <p>{{ article.content }}</p>
+      <router-link :to="{ name: 'Article', params: { id: article.id } }">Lees verder</router-link>
+    </div>
   </article>
 </template>
 
@@ -11,3 +16,33 @@ const { id } = defineProps<{ id: number }>();
 
 const article = articles.find((item) => item.id === id);
 </script>
+
+<style lang="scss">
+@import "../styles/variables.scss";
+
+.news--card {
+  img {
+    border-radius: 0.5rem 0.5rem 0 0;
+    width: 100%;
+    display: block;
+    max-height: 15rem;
+    object-fit: cover;
+  }
+
+  .details {
+    padding: 1.5rem;
+    border-radius: 0 0 0.5rem 0.5rem;
+    border: thin solid rgba(0, 0, 0, 0.2);
+    border-top: none;
+  }
+
+  p {
+    margin: 0.75rem 0;
+  }
+
+  a {
+    color: $primary-color;
+    text-decoration: underline;
+  }
+}
+</style>
