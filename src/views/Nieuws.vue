db0f11d371aea915f14d97784c36a6ce532ff3f9
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-09 18:32:36 +0100

    Added news article

--- src/views/Nieuws.vue
+++ src/views/Nieuws.vue
@@ -1,10 +1,24 @@
 <template>
-  <div v-for="article of articles">
-    <NewsCard :id="article.id" />
-  </div>
+  <Breadcrumbs>Nieuws</Breadcrumbs>
+  <section class="article--section">
+    <div v-for="article of articles">
+      <NewsCard :id="article.id" />
+    </div>
+  </section>
 </template>
 
 <script setup lang="ts">
 import { articles } from "../data/articles";
 import NewsCard from "../components/NewsCard.vue";
+import Breadcrumbs from "../components/Breadcrumbs.vue";
 </script>
+
+<style lang="scss">
+.article--section {
+  display: flex;
+
+  article {
+    flex: 1 1 20rem;
+  }
+}
+</style>
