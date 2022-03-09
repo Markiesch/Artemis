a4b22040ddf0643a2758b4dbbd115fbe21cd6184
Markiesch <marksmeel@gmail.com>
2022-03-09 20:57:34 +0100

    Added date and author data

--- src/data/articles.ts
+++ src/data/articles.ts
@@ -1,5 +1,7 @@
 export interface IArticle {
   id: number;
+  date: string;
+  author: string;
   title: string;
   content: string;
   image: string;
@@ -8,6 +10,8 @@ export interface IArticle {
 export const articles: IArticle[] = [
   {
     id: 3,
+    date: "23 oktober, 2021",
+    author: "Mark Schuurmans",
     title: "Nog een nieuwsartikel!",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo ab nesciunt aperiam adipisci et quas atque voluptatem blanditiis minus similique cumque amet exercitationem commodi, illo minima nemo eaque eligendi.",
@@ -15,6 +19,8 @@ export const articles: IArticle[] = [
   },
   {
     id: 2,
+    date: "28 oktober, 2021",
+    author: "Mark Schuurmans",
     title: "Tweede nieuwsartikel",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo ab nesciunt aperiam adipisci et quas atque voluptatem blanditiis minus similique cumque amet exercitationem commodi, illo minima nemo eaque eligendi.",
@@ -22,6 +28,8 @@ export const articles: IArticle[] = [
   },
   {
     id: 1,
+    date: "4 november, 2021",
+    author: "Mark Schuurmans",
     title: "Eerste nieuwsartikel!",
     content:
       "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus quo ab nesciunt aperiam adipisci et quas atque voluptatem blanditiis minus similique cumque amet exercitationem commodi, illo minima nemo eaque eligendi.",
