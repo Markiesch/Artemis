570a7a4643a875843896b227b5a69090b23240f8
Markiesch <marksmeel@gmail.com>
2022-03-09 20:59:27 +0100

    Added article view

--- src/views/Article.vue
+++ src/views/Article.vue
@@ -1,12 +1,23 @@
 <template>
-  <section v-if="article">
-    <h2>{{ article.title }}</h2>
-  </section>
+  <div v-if="article">
+    <Breadcrumbs label="artikel">
+      <template #default>{{ article.title }}</template>
+      <template #subtitle>
+        <p class="breadcrumbs--subtitle">
+          <span>{{ article.date }}</span> door <a href="">{{ article.author }}</a>
+        </p>
+      </template>
+    </Breadcrumbs>
+    <section class="article--section">
+      <img :src="`/assets/${article.image}`" alt="" />
+    </section>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { useRoute, useRouter } from "vue-router";
 import { articles } from "../data/articles";
+import Breadcrumbs from "../components/Breadcrumbs.vue";
 
 const route = useRoute();
 const router = useRouter();
@@ -14,3 +25,26 @@ const router = useRouter();
 const article = articles.find((item) => item.id === +route.params.id);
 if (!article) router.push({ name: "404" });
 </script>
+
+<style lang="scss">
+.breadcrumbs--subtitle {
+  margin-top: 1.5rem;
+  opacity: 0.9;
+
+  span {
+    font-weight: 600;
+  }
+}
+
+.article--section {
+  padding-top: 2rem;
+
+  img {
+    width: 100%;
+    max-height: 25rem;
+    border-radius: 0.25rem;
+    object-fit: cover;
+    display: block;
+  }
+}
+</style>
