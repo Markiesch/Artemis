83a23c0d42a40955e7794202d5acfa26488f64f2
Markiesch <marksmeel@gmail.com>
2022-03-09 21:59:55 +0100

    Added article list component

--- src/views/News.vue
+++ src/views/News.vue
@@ -1,24 +1,12 @@
 <template>
   <Breadcrumbs>Nieuws</Breadcrumbs>
-  <section class="article--section">
-    <div v-for="article of articles">
-      <NewsCard :id="article.id" />
-    </div>
+  <section>
+    <ArticleList :articles="articles" />
   </section>
 </template>
 
 <script setup lang="ts">
 import { articles } from "../data/articles";
-import NewsCard from "../components/NewsCard.vue";
+import ArticleList from "../components/ArticleList.vue";
 import Breadcrumbs from "../components/Breadcrumbs.vue";
 </script>
-
-<style lang="scss">
-.article--section {
-  display: flex;
-
-  article {
-    flex: 1 1 20rem;
-  }
-}
-</style>
