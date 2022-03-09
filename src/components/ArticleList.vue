cdb741c27a1f5428dbdf0a41133f05f16e779aab
Markiesch <marksmeel@gmail.com>
2022-03-09 21:29:40 +0100

    Added article list component

---
+++ src/components/ArticleList.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="article--wrapper">
+    <div v-for="article of articles">
+      <ArticleCard :id="article.id" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IArticle } from "../data/articles";
+import ArticleCard from "./ArticleCard.vue";
+defineProps<{ articles: IArticle[] }>();
+</script>
+
+<style>
+.article--wrapper {
+  display: flex;
+  margin: -1rem;
+}
+
+article {
+  flex: 1 1 20rem;
+  margin: 1rem;
+}
+</style>
