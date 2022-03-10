d9aebcf46c9786d0cdc3016dc3c192eaac7d5f2d
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-10 12:57:32 +0100

    Added pet data

---
+++ src/data/pets.ts
@@ -0,0 +1,25 @@
+export type IPetFilter = "Honden" | "Katten" | "Overige";
+
+export interface IPet {
+  name: string;
+  filter: IPetFilter;
+  image: string;
+}
+
+export const pets: IPet[] = [
+  {
+    name: "Zana",
+    filter: "Honden",
+    image: "zana.jpg",
+  },
+  {
+    name: "Peggy",
+    filter: "Honden",
+    image: "peggy.jpg",
+  },
+  {
+    name: "Mocha",
+    filter: "Katten",
+    image: "mocha.jpg",
+  },
+];
