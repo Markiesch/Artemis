4af2edffd82422edaa62156f24ba7e3d4b8ae4f1
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-10 12:58:09 +0100

    Added date label

--- src/components/ArticleCard.vue
+++ src/components/ArticleCard.vue
@@ -2,8 +2,9 @@
   <article v-if="article">
     <img :src="`/assets/${article.image}`" :alt="article.title" />
     <div class="details">
+      <p class="date">{{ article.date }}</p>
       <h2>{{ article.title }}</h2>
-      <p>{{ article.content }}</p>
+      <p>{{ article.subtitle }}</p>
       <router-link :to="{ name: 'Article', params: { id: article.id } }">Lees verder</router-link>
     </div>
   </article>
@@ -35,12 +36,26 @@ img {
   border-top: none;
 }
 
+h2 {
+  color: $secondary-color;
+}
+
 p {
   margin: 0.75rem 0;
+
+  color: $text-color;
 }
 
 a {
   color: $primary-color;
   text-decoration: underline;
+}
+
+.date {
+  background-color: rgb(240, 240, 240);
+  display: inline-block;
+  padding: 0.75rem;
+  line-height: 1;
+  border-radius: 0.25rem;
 }
 </style>
