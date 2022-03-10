526e454ac68aab179aa418d27363c861cceedf72
Mark Schuurmans <mark.schuurmans1@edu-kw1c.nl>
2022-03-10 20:41:26 +0100

    Added 404 view

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,3 +1,44 @@
 <template>
-  <h1>404</h1>
+  <section>
+    <h1>404</h1>
+    <p>De opgegeven URL kon niet worden gevonden</p>
+    <div class="link--container">
+      <router-link :to="{ name: 'Home' }">Home</router-link>
+      <router-link :to="{ name: 'Home' }">Overzicht</router-link>
+      <router-link :to="{ name: 'Home' }">Doneren</router-link>
+    </div>
+  </section>
 </template>
+
+<style scoped lang="scss">
+@import "../styles/variables.scss";
+
+section {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+
+h1 {
+  font-size: 7.5rem;
+  color: $title-color;
+}
+
+p {
+  color: $text-color;
+}
+
+.link--container {
+  margin-top: 1rem;
+
+  a {
+    margin: 0.5rem;
+    padding: 1rem;
+    line-height: 1;
+    display: inline-block;
+    background-color: rgb(240, 240, 240);
+    border-radius: 0.5rem;
+  }
+}
+</style>
