7d50edb76582aa1a015f1bfc608eb719ae4e79b7
Markiesch <marksmeel@gmail.com>
2022-03-11 21:36:49 +0100

    Added pet description

--- src/data/pets.ts
+++ src/data/pets.ts
@@ -2,6 +2,7 @@ export type IPetFilter = "Honden" | "Katten" | "Overige";
 
 export interface IPet {
   name: string;
+  description: string;
   filter: IPetFilter;
   image: string;
 }
@@ -9,21 +10,25 @@ export interface IPet {
 export const pets: IPet[] = [
   {
     name: "Zana",
+    description: "TODO",
     filter: "Honden",
     image: "zana.jpg",
   },
   {
     name: "Peggy",
+    description: "TODO",
     filter: "Honden",
     image: "peggy.jpg",
   },
   {
     name: "Mocha",
+    description: "TODO",
     filter: "Katten",
     image: "mocha.jpg",
   },
   {
     name: "Fried chicken",
+    description: "TODO",
     filter: "Honden",
     image: "chickens.jpg",
   },
