4483b4e50fc3e076da6e04990118439ee033d291
Mark Schuurmans <marksmeel@gmail.com>
2022-03-17 13:27:32 +0100

    Added (working) slider dependency

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,5 @@ import App from "./App.vue";
 import Router from "./router";
 import "./styles/variables.scss";
 import "./styles/typography.scss";
-import "@vueform/slider/themes/default.css";
 
 createApp(App).use(Router).mount("body");
