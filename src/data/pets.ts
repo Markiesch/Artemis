c3e7d30c24b5081f1b5010bbb9e6f3e0e1c894bc
Markiesch <marksmeel@gmail.com>
2022-03-19 19:05:54 +0100

    Added gender prop

--- src/data/pets.ts
+++ src/data/pets.ts
@@ -1,6 +1,7 @@
 export interface IPet {
   name: string;
   description: string;
+  gender: string;
   filter: string;
   image: string;
 }
@@ -9,48 +10,56 @@ export const pets: IPet[] = [
   {
     name: "Zana",
     description: "TODO",
+    gender: "Man",
     filter: "Honden",
     image: "zana.jpg",
   },
   {
     name: "Peggy",
     description: "TODO",
+    gender: "Man",
     filter: "Honden",
     image: "peggy.jpg",
   },
   {
     name: "Mocha",
     description: "TODO",
+    gender: "Vrouw",
     filter: "Katten",
     image: "mocha.jpg",
   },
   {
     name: "sammy",
     description: "TODO",
+    gender: "Vrouw",
     filter: "Konijnen",
     image: "sammy.jpg",
   },
   {
     name: "Crosby",
     description: "TODO",
+    gender: "Man",
     filter: "Honden",
     image: "crosby.jpg",
   },
   {
     name: "Jamie",
     description: "TODO",
+    gender: "Man",
     filter: "Honden",
     image: "jamie.jpg",
   },
   {
     name: "Sassy",
     description: "TODO",
+    gender: "Vrouw",
     filter: "Katten",
     image: "sassy.jpg",
   },
   {
     name: "Gavin",
     description: "TODO",
+    gender: "Man",
     filter: "Konijnen",
     image: "gavin.jpg",
   },
